8f318adddcead87e4eac3d6808ace41ff020b9c0
Daniel Criado <daniel@passnfly.com>
2020-04-02 14:09:51 +0200

    Initial commit

---
+++ grpc-services/users/server/server.py
@@ -0,0 +1,79 @@
+import sys
+import time
+from concurrent import futures
+
+sys.path.append('../../protos/gen-py')
+import grpc
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+
+import users_pb2_grpc as users_service
+import users_types_pb2 as users_messages
+
+import airlines_pb2_grpc as airlines_service
+import airlines_types_pb2 as airlines_messages
+
+_ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+
+class UsersService(users_service.UsersServicer):
+
+    def CreateUser(self, request, context):
+        metadata = dict(context.invocation_metadata())
+        print(metadata)
+        user = users_messages.User(username=request.username, user_id=1)
+        return users_messages.CreateUserResult(user=user)
+
+    def GetUsers(self, request, context):
+        for user in request.user:
+            user = users_messages.User(
+                username=user.username, user_id=user.user_id
+            )
+            yield users_messages.GetUsersResult(user=user)
+
+
+class AirlinesService(airlines_service.AirlinesServicer):
+    CORE_DB_URI = 'mysql+pymysql://root:root@127.0.0.1/PASSNFLY'
+
+    # Metadata
+    metadata = db.MetaData()
+
+    # AUTOMATION
+    core_engine = db.create_engine(CORE_DB_URI)
+    CoreSession = sessionmaker(bind=core_engine)
+    core_session = CoreSession()
+    AIRLINE = db.Table('AIRLINE', metadata, autoload=True, autoload_with=core_engine)
+
+    def GetAirline(self, request, context):
+        filters = {'icao': request.icao, 'iata': request.iata}
+        airline = self.core_session.query(self.AIRLINE).filter_by(**filters).first()
+        if airline:
+            airline = airlines_messages.Airline(icao=airline.icao, iata=airline.iata, name=airline.name)
+        else:
+            airline = airlines_messages.Airline(icao='', iata='', name='')
+        return airlines_messages.GetAirlineResult(airline=airline)
+
+    def GetAirlines(self, request, context):
+        airlines = self.core_session.query(self.AIRLINE.columns.icao, self.AIRLINE.columns.iata, self.AIRLINE.columns.name)
+        for airline in airlines:
+            airline = airlines_messages.Airline(
+                icao=airline.icao, iata=airline.iata, name=airline.name
+            )
+            yield airlines_messages.GetAirlineResult(airline=airline)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    users_service.add_UsersServicer_to_server(UsersService(), server)
+    airlines_service.add_AirlinesServicer_to_server(AirlinesService(), server)
+    server.add_insecure_port('127.0.0.1:50061')
+    server.start()
+    try:
+        while True:
+            time.sleep(_ONE_DAY_IN_SECONDS)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == '__main__':
+    serve()
---
+++ webapp/app.py
@@ -0,0 +1,58 @@
+from flask import Flask, Response, jsonify, json
+import sys
+
+sys.path.extend(['../grpc-services/protos/gen-py', '../grpc-services'])
+from google.protobuf.json_format import MessageToJson
+from client_wrapper import ServiceClient
+
+import users_pb2_grpc as users_service
+import users_types_pb2 as users_messages
+
+import airlines_pb2_grpc as airlines_service
+import airlines_types_pb2 as airlines_messages
+
+app = Flask(__name__)
+app.config['users'] = ServiceClient(users_service, 'UsersStub', 'localhost', 50061)
+app.config['airlines'] = ServiceClient(airlines_service, 'AirlinesStub', 'localhost', 50061)
+
+
+@app.route('/users/')
+def users_get():
+    request = users_messages.GetUsersRequest(
+        user=[users_messages.User(username="alexa", user_id=1),
+              users_messages.User(username="christie", user_id=1)
+              ]
+    )
+
+    response_list = []
+
+    def get_users():
+        response = app.config['users'].GetUsers(request)
+        for resp in response:
+            response_list.append(json.loads(MessageToJson(resp.user)))
+
+    get_users()
+    return jsonify(response_list)
+
+
+@app.route('/airline/<string:icao>/<string:iata>')
+def airline_get(icao, iata):
+    request = airlines_messages.GetAirlineRequest(icao=icao, iata=iata)
+
+    response = app.config['airlines'].GetAirline(request)
+    json.loads(MessageToJson(response.airline))
+    return jsonify(json.loads(MessageToJson(response.airline)))
+
+
+
+@app.route('/airlines/')
+def airlines_get():
+    response_list = []
+    response = app.config['airlines'].GetAirlines(airlines_messages.NoParameters())
+    for resp in response:
+        response_list.append(json.loads(MessageToJson(resp.airline)))
+    return jsonify(response_list)
+
+
+if __name__ == '__main__':
+    app.run()
